ea14e189bbfb4773332797ce27488ff5d3b3ec3f
Alessandro Maestri <umpire274@303487.no-reply.drupal.org>
2021-02-23 12:18:44 +0100

    Correction of inversion of calculation of conversion functions inHg <-> hPa

--- metar_taf_parser/commons/converter.py
+++ metar_taf_parser/commons/converter.py
@@ -25,11 +25,11 @@ def convert_temperature(input):
 
 
 def convert_inhg_to_hpa(input):
-    return '{:5.2f}'.format(float(input) * 0.029529983071445)
+    return '{:4.0f}'.format(float(input) / 0.029529983071445)
 
 
 def convert_hpa_to_inhg(input):
-    return '{:4.0f}'.format(float(input) / 0.029529983071445)
+    return '{:5.2f}'.format(float(input) * 0.029529983071445)
 
 
 def convert_pressure(input):
