9169b7d107542da2520144a6f5c2ab1e7230b831
Alessandro Maestri <umpire274@303487.no-reply.drupal.org>
2021-02-23 12:21:21 +0100

    Deleted import statement not used

--- tests/common/test_converter.py
+++ tests/common/test_converter.py
@@ -2,7 +2,6 @@ import unittest
 
 from parameterized import parameterized
 from metar_taf_parser.commons import converter
-from metar_taf_parser.model.model import Pressure
 
 
 class ConverterTest(unittest.TestCase):
