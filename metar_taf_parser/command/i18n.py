f878bfdfd8a75cce9ac53b57303f57928486518c
jk KPADEY <kpadeyjk@gmail.com>
2021-03-11 12:58:06 +0100

    Moved i18n module to commons package

--- metar_taf_parser/command/i18n.py
+++
@@ -1,6 +0,0 @@
-import gettext
-import os
-
-localedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../locale')
-t = gettext.translation(domain='messages', localedir=localedir, fallback=True, languages=['en', 'fr', 'pl', 'de'])
-_ = t.gettext
