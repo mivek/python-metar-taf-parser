8ed35f4b29191ebdfb150e31e4a7c0f174aed8aa
Alessandro Maestri <umpire274@303487.no-reply.drupal.org>
2021-07-31 21:25:58 +0200

    Issue #17
    Updated like suggested

--- metar_taf_parser/commons/i18n.py
+++ metar_taf_parser/commons/i18n.py
@@ -9,7 +9,7 @@ localedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../locale'
 langAvailable = os.listdir(localedir)
 defLocale = locale.getdefaultlocale()
 langCounty = defLocale[0]
-lang = langCounty[:2]
+lang = locale.getdefaultlocale()[0][:2]
 if lang not in langAvailable:
     lang = 'en'
 t = gettext.translation(domain='messages', localedir=localedir, fallback=True, languages=[lang])
