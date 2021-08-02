c64231b509d054f73320f3632113c61a84b9dec8
Alessandro Maestri <umpire274@303487.no-reply.drupal.org>
2021-08-02 14:10:20 +0200

    Issue #17
    Deleted rows unused

--- metar_taf_parser/commons/i18n.py
+++ metar_taf_parser/commons/i18n.py
@@ -7,8 +7,6 @@ from metar_taf_parser.commons.exception import TranslationError
 
 localedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../locale')
 langAvailable = os.listdir(localedir)
-defLocale = locale.getdefaultlocale()
-langCounty = defLocale[0]
 lang = locale.getdefaultlocale()[0][:2]
 if lang not in langAvailable:
     lang = 'en'
