8b224372918f47fc6f3ba210c75fe8acc662427f
Alessandro <umpire274@gmail.com>
2021-08-08 20:07:58 +0200

    Solved issue about right locale (#17)
    * #14
    
    This solution solve the problem for the use of right locale instead the default (en)
    
    * Issue #14
    
    This solution solve the problem for the use of right locale instead the default (en)
    
    * Issue #17
    
    Updated like suggested
    
    * Issue #17
    
    Deleted rows unused
    
    Co-authored-by: Alessandro Maestri <umpire274@303487.no-reply.drupal.org>

--- metar_taf_parser/commons/i18n.py
+++ metar_taf_parser/commons/i18n.py
@@ -1,11 +1,16 @@
 import gettext
+import locale
 import os
 
 from metar_taf_parser.commons.exception import TranslationError
 
 
 localedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../locale')
-t = gettext.translation(domain='messages', localedir=localedir, fallback=True, languages=['en', 'fr', 'pl', 'de', 'it'])
+langAvailable = os.listdir(localedir)
+lang = locale.getdefaultlocale()[0][:2]
+if lang not in langAvailable:
+    lang = 'en'
+t = gettext.translation(domain='messages', localedir=localedir, fallback=True, languages=[lang])
 
 
 def _(message: str) -> str:
